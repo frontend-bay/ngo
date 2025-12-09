8af9f136e430431afdb04042adca03f666fea8ac
amit96pal <amit96pal.ap@gmail.com>
2025-12-09 14:26:09 +0530

    rename folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import OrganizationIntro from './organization/OrganizationIntro'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import NTIntro from "./nirmal-transport/NTIntro.jsx";
+import NTIntro from "./nirmaltransport/NTIntro.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
